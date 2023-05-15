7ece198f46f0d55f2d021a52a5094decef4352c3
Wesley Fernandes <wesleyintelligence@protonmail.com>
2023-05-15 05:59:52 -0300

    git import fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import Navbar from '@module/components/Navbar'
-import './globals.css'
+import './globals.scss'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
